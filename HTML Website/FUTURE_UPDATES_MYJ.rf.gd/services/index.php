43535a3cfbeb115a1bcd8b9892fc04645f978d73
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-12-20 20:45:35 +0300

    Added calculator

---
+++ HTML Website/FUTURE_UPDATES_MYJ.rf.gd/services/index.php
@@ -0,0 +1,74 @@
+<html>
+
+<head>
+    <link rel="icon" href="../MYJ_icon_updated.png">
+    <link rel="stylesheet" href="style.css">
+    <title>MYJ Services</title>
+</head>
+
+<body>
+    <div id="back-mobile">
+        <a href="https://myj.rf.gd"><button>&#x25c0; Back to Home</button></a>
+    </div>
+
+    <!-- <p id="coming-soon">Coming Soon</p> -->
+
+    <div class="cards">
+        <?php
+            $host="sql112.epizy.com";
+            $dbuser="epiz_30389702";
+            $dbpassword="VjgiAEJdCn4";
+            $dbname="epiz_30389702_database";
+            $conn = new mysqli($host, $dbuser, $dbpassword, $dbname);
+            $success = "Success!";
+            $fail = "Not Connected";
+            if($conn) {
+                echo "<script>console.log('{$success}' );</script>";
+            }
+            else {
+                echo "<script>console.log('{$fail}' );</script>";
+            }
+            $query = $conn->query("SELECT * FROM services");
+
+            while ($row=$query->fetch_array(MYSQLI_ASSOC)) {
+                echo "<div class='card1'>";
+                echo "<div class='left-card1'>";
+                echo "<img src='".$row['logo']."' alt='".$row['name']."' width='80px'>";
+                echo "<p>".$row['name']."</p>";
+                echo "<a href='".$row['url']."'><button>Try service ></button></a>";
+                echo "</div>";
+                echo "<div class='right-card1'>";
+                echo "<h2>".$row['name']."</h2>";
+                echo "<p>Description: ".$row['description']."</p>";
+                echo "</div>";
+            }
+        ?>
+    </div>
+
+    <script type='text/javascript'>
+        (function(I, L, T, i, c, k, s) {
+            if (I.iticks) return;
+            I.iticks = {
+                host: c,
+                settings: s,
+                clientId: k,
+                cdn: L,
+                queue: []
+            };
+            var h = T.head || T.documentElement;
+            var e = T.createElement(i);
+            var l = I.location;
+            e.async = true;
+            e.src = (L || c) + '/client/inject-v2.min.js';
+            h.insertBefore(e, h.firstChild);
+            I.iticks.call = function(a, b) {
+                I.iticks.queue.push([a, b]);
+            };
+        })(window, 'https://cdn-v1.intelliticks.com/prod/common', document, 'script', 'https://app.intelliticks.com', '24dSCuYprvQfPp73d_c', {});
+    </script>
+</body>
+
+
+
+
+</html>
