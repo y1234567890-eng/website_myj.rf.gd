09cb1167596ae310c6e3524cdeed6b21cab0a686
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-12-21 19:20:45 +0300

    Added mobile support to services.php

--- HTML Website/FUTURE_UPDATES_MYJ.rf.gd/services/index.php
+++ HTML Website/FUTURE_UPDATES_MYJ.rf.gd/services/index.php
@@ -3,6 +3,8 @@
 <head>
     <link rel="icon" href="../MYJ_icon_updated.png">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width">
     <title>MYJ Services</title>
 </head>
 
