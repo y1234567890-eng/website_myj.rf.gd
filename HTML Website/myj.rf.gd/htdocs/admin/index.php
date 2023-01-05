94682f76ae6c40b6d9d7a45cf6b9282f282fe820
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2023-01-05 15:51:07 +0300

    Updated admin.php

--- HTML Website/myj.rf.gd/htdocs/admin/index.php
+++ HTML Website/myj.rf.gd/htdocs/admin/index.php
@@ -2,7 +2,7 @@
     <head>
         <link rel="icon" href="https://www.myj.rf.gd/MYJ_icon_updated.png">
         <meta name="viewport" content="width=device-width">
-        <title>Feedback Form</title>
+        <title>Admin login</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300;400&display=swap');
         body {
@@ -24,7 +24,6 @@
             padding: 20px;
         }
         #back-mobile {
-            /* display: none; */
             margin: 10px;
             position: fixed;
         }
@@ -82,11 +81,87 @@
             border: 2px solid #4CAF50;
         }
         .default-box {
-            display: none;
+            /* display: none; */
+        }
+        input[type=text] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 2px 0;
+            box-sizing: border-box;
+            border: 3px solid #ccc;
+            -webkit-transition: 0.5s;
+            transition: 0.5s;
+            outline: none;
+        }
+
+        input[type=text]:focus {
+            border: 3px solid #555;
+        }
+
+        input[type=email] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 2px 0;
+            box-sizing: border-box;
+            border: 3px solid #ccc;
+            -webkit-transition: 0.5s;
+            transition: 0.5s;
+            outline: none;
+        }
+
+        input[type=email]:focus {
+            border: 3px solid #555;
+        }
+
+        input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 2px 0;
+            box-sizing: border-box;
+            border: 3px solid #ccc;
+            -webkit-transition: 0.5s;
+            transition: 0.5s;
+            outline: none;
+        }
+
+        input[type=password]:focus {
+            border: 3px solid #555;
+        }
+
+        input[type=submit] {
+            background-color: #04AA6D;
+            border: none;
+            color: #FFFFFF;
+            padding: 16px 32px;
+            text-decoration: none;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 8px;
+            transition-duration: 0.4s;
+            border: 2px solid #4CAF50;
+        }
+
+        input[type=submit]:hover {
+            transform: scale(1.1);
+            background-color: #ffffff;
+            color: #000000;
+            border: 2px solid #4CAF50;
         }
     </style>
     </head>
     <body>
+        <div id="back-mobile">
+            <a href="https://myj.rf.gd"><button>&#x25c0; Back to Home</button></a>
+        </div>
+        
+        <h1>Admin Login</h1>
+        <div id="log-in-form">
+            <form method="POST">
+                <input type="text" name="username" placeholder="Username" required><br>
+                <input type="password" name="password" placeholder="Password" required><br>
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        <div>
         <?php
             $host="sql112.epizy.com";
             $dbuser="epiz_30389702";
@@ -95,6 +170,7 @@
             $conn = new mysqli($host, $dbuser, $dbpassword, $dbname);
             $success = "Success!";
             $fail = "Not Connected";
+            $correct = "false";
             if($conn) {
                 echo "<script>console.log('{$success}' );</script>";
             }
@@ -105,51 +181,31 @@
             if(isset($_POST['submit'])) {
                 $username=$conn->real_escape_string($_POST['username']);
                 $password=$conn->real_escape_string($_POST['password']);
-                // $date=date('d/m/y h:i:s',strtotime('+4 hour'));
-                // $conn->query("INSERT INTO feedback (title,details,date) VALUES ('$title','$details','$date')");
 
-                // $sender = "contact.us.myj@gmail.com"
-                // mail($sender,$name,$message,$email)
 
                 $query = $conn->query("SELECT * FROM adminaccounts");
-                // $num = $query->num_rows();
-                // ORDER BY DESC;
+                
                 while ($row=$query->fetch_array(MYSQLI_ASSOC)) {
-                    // echo "<table border='2'>";
-                    // echo "<tr><td>";
-                    // echo "<h1>".$row['name']."</h1></td></tr>";
-                    // echo "<tr><td>".$row['message']."<br><br></td></tr>";
-                    // echo "<tr><td><b>Admin reply:</b><br><p style='text-align: left;'>Dear User,<br>".$row['reply']."</p></td></tr>";
-                    // echo "</table>";
-                    if ($username==.row['title']) {
-                        if($password==.row['password']) {
-                            echo "<script>document.getElementById('logged-in').style.display = 'box';</script>";
-                            echo "<script>document.getElementById('log-in-form').style.display = 'none';</script>";
-                        }
+                    if ($username==$row['username']) {
+                        if ($password == $row['password']) {
+                            $correct = "true";
+                            echo "<div id='logged-in'>";
+                            echo "<div id='new-feature' class='default-box'>";
+                            echo "<h2>Add Update</h2>";
+                            echo "<div id='add-update' class='default-btn'>";
+                            echo "<a href='new'><button>New Update</button></a>";
+                            echo "</div></div></div>";
+                            echo "<script>document.getElementById('log-in-form').style.display = none;</script>"
+                        } else {
+                            $correct="false";
+                            echo "Password does not match with the username";                        }
+                    } else {
+                        $correct="false";
+                        echo "<p>Username does not exist</p>";
                     }
                 }
             }
         ?>
-        <div id="back-mobile">
-            <a href="https://myj.rf.gd"><button>&#x25c0; Back to Home</button></a>
-        </div>
-        
-        <h1>Admin Login</h1>
-        <div id="logged-in">
-            <div id="new-feature" class="default-box">
-                <h2>Add Update</h2>
-                <div id="add-update" class="default-btn">
-                    <a href="new"><button>New Update</button></a>
-                </div>
-            </div>
-        </div>
-        <div id="log-in-form">
-            <form method="POST">
-                <input type="text" name="username" placeholder="Username" required><br>
-                <input type="password" name="password" placeholder="Password" required><br>
-                <input type="submit" name="submit" value="Submit">
-            </form>
-        <div>
         <script>
             if(window.history.replaceState) {
                 window.history.replaceState(null, null, window.location.href);
